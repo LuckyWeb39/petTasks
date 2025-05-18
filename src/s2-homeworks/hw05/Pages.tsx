ca0092c2a7d3093af7aafc6b36c4e15ff8afa5c4
Alexander_Lukomskiy <lukomskiy.ac@gmail.com>
2025-05-18 18:51:09 +0200

    add navBar and SideBar

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,17 +16,17 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path='/' element={<Navigate to={'/pre-junior'}/>}/>
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+
+                <Route path='/pre-junior' element={<PreJunior/>} />
+                <Route path='/junior' element={<Junior/>} />
+                <Route path='/junior-plus' element={<JuniorPlus/>} />
+
+
+                <Route path='/Error404' element={<Error404/>} />
+                <Route path='/*' element={<Navigate to={'/Error404'}/>} />
             </Routes>
         </div>
     )
